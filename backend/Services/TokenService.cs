322605fc420bc3ae72ce751994e73728d4bba491
d1av <davi4alves@gmail.com>
2023-01-04 17:29:09 -0300

    feat: add token logic and claims

--- backend/Services/TokenService.cs
+++ backend/Services/TokenService.cs
@@ -1,13 +1,39 @@
-﻿using DatingApp.Entities;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using DatingApp.Entities;
 using DatingApp.Interface;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.Services
 {
     public class TokenService : ITokenService
     {
-        public string CreateToken(AppUser appUser)
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
         {
-            throw new NotImplementedException();
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
         }
     }
 }
+
+
