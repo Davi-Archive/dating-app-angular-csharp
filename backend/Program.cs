a4fc5ad18ea8879bf0cab14c4febe70b4382f733
d1av <davi4alves@gmail.com>
2023-01-04 00:49:17 -0300

    feat: cors configuration in program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -14,9 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title: string = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:8080/api/users").subscribe({
+      next: (res) => this.users = res,
+      error: err => console.log(err),
+      complete: () => console.log('Request has completed')
+    })
+  }
 }
