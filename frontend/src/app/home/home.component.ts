1393466a1dae5272232d644c08286951579d7492
d1av <davi4alves@gmail.com>
2023-01-07 17:49:47 -0300

    style: add user card with animations

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -13,23 +12,11 @@ export class HomeComponent implements OnInit {
 
   url: string = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
-
-  getUsers() {
-    this.http.get(this.url + "users").subscribe({
-      next: (res) => {
-        this.users = res
-        console.log(res);
-      },
-      error: err => console.log(err),
-      complete: () => console.log('Request has completed')
-    })
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
