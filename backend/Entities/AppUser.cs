59e3a7ebd9df1ac92386968c7488659649e75189
d1av <davi4alves@gmail.com>
2023-01-07 02:55:49 -0300

    feat: add entity photo and more properties on user

--- backend/Entities/AppUser.cs
+++ backend/Entities/AppUser.cs
@@ -1,10 +1,26 @@
-﻿namespace DatingApp.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatingApp.Entities
 {
     public class AppUser
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PaswordHash { get; set; }
         public byte[] PaswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new List<Photo>();
     }
 }
---
+++ backend/Entities/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatingApp.Entities
+{
+    public class Photo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
