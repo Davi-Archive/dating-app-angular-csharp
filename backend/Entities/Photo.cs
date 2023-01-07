b5c5c505510ac53429f4dae714ca3a1162e1cc75
d1av <davi4alves@gmail.com>
2023-01-07 12:08:48 -0300

    feat: add database seeding from code

--- backend/Entities/Photo.cs
+++ backend/Entities/Photo.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DatingApp.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         [Key]
@@ -11,5 +12,7 @@ namespace DatingApp.Entities
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,9 @@
+using DatingApp.Data;
 using DatingApp.Extensions;
 using DatingApp.Interface;
 using DatingApp.Middleware;
 using DatingApp.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,5 +45,21 @@ app.UseAuthorization();
 
 
 app.MapControllers();
+
+// seed database data
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
