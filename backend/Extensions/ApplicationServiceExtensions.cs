137d2abab69b961f5f3395dc4bcbc76b76a5adc4
d1av <davi4alves@gmail.com>
2023-01-08 15:13:56 -0300

    feat: add cloudinary support and photo service

--- backend/Extensions/ApplicationServiceExtensions.cs
+++ backend/Extensions/ApplicationServiceExtensions.cs
@@ -1,7 +1,8 @@
-﻿using DatingApp.Data;
-using DatingApp.Helpers;
+﻿using CloudinaryDotNet;
+using DatingApp.Data;
 using DatingApp.Interface;
 using DatingApp.Services;
+using dotenv.net;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Extensions
@@ -15,11 +16,13 @@ namespace DatingApp.Extensions
             services.AddSwaggerGen();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
-            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            DotEnv.Load(options: new DotEnvOptions(probeForEnv: true));
+            Cloudinary cloudinary = new(Environment.GetEnvironmentVariable("CLOUDINARY_URL"));
+            services.AddScoped<IPhotoService, PhotoService>();
+            cloudinary.Api.Secure = true;
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-                // opt.UseInMemoryDatabase("DatingApp");
             });
 
             return services;
---
+++ backend/Services/PhotoService.cs
@@ -0,0 +1,47 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.Helpers;
+using DatingApp.Interface;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public PhotoService(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account
+                (
+                Environment.GetEnvironmentVariable("CLOUDINARY_URL")
+                );
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation()
+                    .Height(500).Width(500).Crop("fill").Gravity("face"),
+                    Folder = "da-net7"
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+
+            return await _cloudinary.DestroyAsync(deleteParams);
+        }
+    }
+}
