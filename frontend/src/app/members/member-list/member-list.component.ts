95b99dc2271c8894205e905c806902ec7feaa918
d1av <davi4alves@gmail.com>
2023-01-08 01:12:59 -0300

    feat: add observables to save objects in a global state and no recall api

--- frontend/src/app/members/member-list/member-list.component.ts
+++ frontend/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/services/members.service';
 
@@ -8,7 +9,7 @@ import { MembersService } from 'src/app/services/members.service';
   styleUrls: ['./member-list.component.scss']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberService: MembersService) { }
 
@@ -16,10 +17,6 @@ export class MemberListComponent implements OnInit {
     this.loadMembers();
   }
   loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.members$ = this.memberService.getMembers();
   }
-
-
 }
