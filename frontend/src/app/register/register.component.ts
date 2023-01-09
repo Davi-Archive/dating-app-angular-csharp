db17a0fa5a5690b9be764c9d609e0069318b07e4
d1av <davi4alves@gmail.com>
2023-01-08 23:50:42 -0300

    feat: add Reactive forms with validation

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
@@ -10,22 +11,43 @@ import { AccountService } from '../services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', [Validators.required]),
+      password: new FormControl('', [Validators.required,
+      Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    })
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel()
-      },
-      error: err => {
-        console.log(err);
-        this.toastr.error(err.erro)
-      }
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel()
+    //   },
+    //   error: err => {
+    //     console.log(err);
+    //     this.toastr.error(err.erro)
+    //   }
+    // })
   }
   cancel() {
     this.cancelRegister.emit(false);
