9937271effecf46d7d34c23ac7a54e632e7a8a6b
d1av <davi4alves@gmail.com>
2023-01-16 15:55:21 -0300

    feat: add go to message tab when click envelope, not functional yet

--- frontend/src/app/members/member-detail/member-detail.component.ts
+++ frontend/src/app/members/member-detail/member-detail.component.ts
@@ -13,7 +13,7 @@ import { MessageService } from 'src/app/services/message.service';
   styleUrls: ['./member-detail.component.scss']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
+  @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
   member: Member | undefined;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
@@ -27,6 +27,12 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember()
+
+    this.route.queryParams.subscribe({
+      next: params => {
+        params['tab'] && this.selectTab(params['tab'])
+      }
+    })
 
     this.galleryOptions = [
       {
@@ -63,6 +69,12 @@ export class MemberDetailComponent implements OnInit {
           this.galleryImages = this.getImages()
       }
     })
+  }
+
+  selectTab(heading: string) {
+    if (this.memberTabs) {
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+    }
   }
 
   loadMessages() {
