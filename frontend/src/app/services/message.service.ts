fb10dde84c16910d9cb0e528d3c0a7ee6bdd8d26
d1av <davi4alves@gmail.com>
2023-01-16 13:17:13 -0300

    feat: message service and component

---
+++ frontend/src/app/services/message.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Message } from '../models/message';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMessages(pageNumber: number, pageSize: number, container: string) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
+}
