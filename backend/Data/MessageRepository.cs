1d5083a469e9f51120af017c312b58fe508a81aa
d1av <davi4alves@gmail.com>
2023-01-16 19:34:10 -0300

    feat: add api message deletion from both users

--- backend/Data/MessageRepository.cs
+++ backend/Data/MessageRepository.cs
@@ -43,9 +43,12 @@ namespace DatingApp.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null),
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username
+                && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username
+                && u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username &&
+               && u.RecipientDeleted == false && u.DateRead == null),
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -61,8 +64,10 @@ namespace DatingApp.Data
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
                     m => m.RecipientUsername == currentUserName &&
+                    m.RecipientDeleted == false &&
                     m.SenderUsername == recipientUserName ||
                     m.RecipientUsername == recipientUserName &&
+                    m.SenderDeleted == false &&
                     m.SenderUsername == currentUserName
                 )
                 .OrderBy(m => m.MessageSent)
