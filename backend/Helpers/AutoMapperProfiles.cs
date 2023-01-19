903dd4f33d826efb5e60c7678e50f9400bdef5fd
d1av <davi4alves@gmail.com>
2023-01-18 21:37:06 -0300

    feat: configuring date time

--- backend/Helpers/AutoMapperProfiles.cs
+++ backend/Helpers/AutoMapperProfiles.cs
@@ -23,6 +23,11 @@ namespace DatingApp.Helpers
                 .ForMember(d => d.RecipientPhotoUrl,
                 o => o.MapFrom(s => s.Recipient.Photos
                 .FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<DateTime, DateTime>()
+                .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+            CreateMap<DateTime?, DateTime?>()
+                .ConvertUsing(d => d.HasValue ? DateTime
+                .SpecifyKind(d.Value, DateTimeKind.Utc) : null);
         }
     }
 }
