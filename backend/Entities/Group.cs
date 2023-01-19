3f53c3e08f95f41e42d951a1fd549a816c054a1b
d1av <davi4alves@gmail.com>
2023-01-18 21:31:17 -0300

    feat: add group and time of read

--- backend/Entities/Group.cs
+++ backend/Entities/Group.cs
@@ -4,6 +4,13 @@ namespace DatingApp.Entities
 {
     public class Group
     {
+        private string groupName;
+
+        public Group(string groupName)
+        {
+            this.groupName = groupName;
+        }
+
         [Key]
         public string Name { get; set; }
         public ICollection<Connection> Connections { get; set; } = new List<Connection>();
